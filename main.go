3b8f080443dbbe5fe786a2e14679652dce1ed4e8
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2022-02-15 13:44:38 -0500

    print prompt lines

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
 	"log"
 
@@ -12,8 +14,24 @@ func makePrompt(s ssh.Session) string {
 }
 
 func sshHandler(s ssh.Session) {
+	reader := bufio.NewReader(s)
 	io.WriteString(s, makePrompt(s))
-	io.ReadAll(s)
+	for {
+		oneByte, err := reader.ReadByte()
+		fmt.Printf("%#v\n", oneByte)
+		if err != nil {
+			return
+		}
+		if oneByte == '\x04' {
+			s.Close()
+			return
+		}
+		io.WriteString(s, string(oneByte))
+		if oneByte == '\x0d' {
+			io.WriteString(s, string('\n'))
+			io.WriteString(s, makePrompt(s))
+		}
+	}
 }
 
 func main() {
