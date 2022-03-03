5817bfda875373a94984278c16fc623356c60d8c
Kyle Sferrazza <git@kylesferrazza.com>
2022-03-03 12:40:29 -0500

    gow

--- flake.nix
+++ flake.nix
@@ -5,11 +5,15 @@
     pkgs = import nixpkgs {
       system = "x86_64-linux";
     };
+    gow = pkgs.writeShellScriptBin "gow" ''
+      ${pkgs.findutils}/bin/find . -name "*.go" | ${pkgs.entr}/bin/entr -r go "$@"
+    '';
   in {
     devShell.x86_64-linux = pkgs.mkShell {
       name = "ssh";
       buildInputs = with pkgs; [
         go
+        gow
         gopls
       ];
     };
