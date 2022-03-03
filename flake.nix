88d39f6893a44f40d56fde367b262d7c2bbacbf3
Kyle Sferrazza <git@kylesferrazza.com>
2022-03-03 12:52:44 -0500

    add conn

--- flake.nix
+++ flake.nix
@@ -8,6 +8,13 @@
     gow = pkgs.writeShellScriptBin "gow" ''
       ${pkgs.findutils}/bin/find . -name "*.go" | ${pkgs.entr}/bin/entr -r go "$@"
     '';
+    conn = pkgs.writeShellScriptBin "conn" ''
+      while true; do
+        ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R "[localhost]:2222"
+        ${pkgs.sshpass}/bin/sshpass -p 'password' ssh -p 2222 localhost -o stricthostkeychecking=no
+        sleep 2
+      done
+    '';
   in {
     devShell.x86_64-linux = pkgs.mkShell {
       name = "ssh";
@@ -15,6 +22,7 @@
         go
         gow
         gopls
+        conn
       ];
     };
   };
